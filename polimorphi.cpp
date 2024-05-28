e51319f6073311e64ef438fdd9ca38fce140a8dc
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-28 15:04:52 +0700

    membuat virtual void

--- polimorphi.cpp
+++ polimorphi.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 
 class seseorang {
-    public
-}
+    public:
+    virtual void pesan() = 0;
+    //virtual void pesan(){
+     //   cout << "pesan dari seseorang" << endl;
+    //}
+};
