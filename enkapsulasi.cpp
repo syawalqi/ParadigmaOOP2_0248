f7381acdde002f47d1b57bc06057b29ed02e5320
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-28 15:48:14 +0700

    membuat int main

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -12,3 +12,19 @@ class remoteLampu{
         return saklarNo[i];
     }
 };
+
+int main() {
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarlampu(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarlampu(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarlampu(2, "Lampu Teras Tidur");
+    lampuRumah.setSaklarlampu(3, "Lampu Dapur");
+
+    cout << lampuRumah.getSaklarNo(0) << endl;
+    cout << lampuRumah.getSaklarNo(1) << endl;
+    cout << lampuRumah.getSaklarNo(2) << endl;
+    cout << lampuRumah.getSaklarNo(3) << endl;
+    retun 0;
+ 
+}
