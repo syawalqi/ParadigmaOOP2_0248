de508c7f82d31a8aa5df6d1c7db965acf9726245
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-28 15:38:48 +0700

    membuat noOveridding

--- noOveridding.cpp
+++ noOveridding.cpp
@@ -1,2 +1,24 @@
 #include <iostream>
 using namespace std;
+
+class baseClass {
+    public :
+    virtual void perkenalan() final {
+    
+        cout << "hello saya Function dari base class";
+    }
+};
+
+class derivedClass : public baseClass {
+    public: 
+    void perkenalan() 
+    {
+        cout << "hello saya function dari derived class";
+    }
+};
+
+int main() {
+    derivedClass a;
+    a.perkenalan();
+    return 0;
+}
